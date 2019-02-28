d0df7545cbefa56933956c412acdf3dc4a433389
Charles-Lewis jaggi <charles-lewis.jaggi@he-arc.ch>
2019-02-28 11:45:29 +0100

    init projet

---
+++ src/main/java/ch/hearc/sandbox/SmfApplication.java
@@ -0,0 +1,13 @@
+package ch.hearc.sandbox;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmfApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SmfApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ch/hearc/sandbox/SmfApplicationTests.java
@@ -0,0 +1,16 @@
+package ch.hearc.sandbox;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SmfApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
